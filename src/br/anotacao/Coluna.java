1d6646b1186525a00ed42f3de294acb382ff4dc5
Flávio Rocha Cezarino <ghostreapedrc@gmail.com>
2016-04-20 21:47:27 -0300

    Inicio do projeto

---
+++ src/br/anotacao/Coluna.java
@@ -0,0 +1,18 @@
+package br.anotacao;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Coluna {
+
+	String nome() default "";
+
+	int tamanho() default -1;
+	
+	boolean pk() default false;
+
+}
